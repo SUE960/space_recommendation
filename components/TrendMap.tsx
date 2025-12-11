a70fef0bee0c5d828c05cfe5a54f2393d63ca783
SUE <96976413+SUE960@users.noreply.github.com>
2025-12-12 00:26:59 +0900

    추천받기 버튼을 질문 페이지로 연결 및 실제 데이터 기반 추천 로직 개선

--- components/TrendMap.tsx
+++ components/TrendMap.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import { useState } from 'react'
-import Map, { Marker, Popup } from 'react-map-gl'
-import type { MapRef } from 'react-map-gl'
+import Map from 'react-map-gl'
+import { Marker, Popup } from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import styles from './TrendMap.module.css'
 
